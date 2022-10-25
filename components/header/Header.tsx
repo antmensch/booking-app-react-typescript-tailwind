a0339b9577bb081e24750a852be344baec6318bf
Anton Sydorenko <an.sydorenko@gmail.com>
2022-10-25 12:08:09 +0300

    working on header UI

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../public/logo.svg";
+import { BsSun, BsMoon } from "react-icons/bs";
 
 function Header() {
     return (
-        <div className="flex h-20 w-full items-center">
+        <div className="flex h-20 w-full items-center bg-white">
             <header className="container sticky mx-auto flex justify-between">
                 <Link href="/">
                     <a className="flex items-center justify-around">
@@ -14,8 +15,30 @@ function Header() {
                     </a>
                 </Link>
                 <div className="flex items-center justify-between">
-                    <div>Night mode toggle</div>
-                    <div>buttons group</div>
+                    <div className="flex items-center justify-center">
+                        <input type="checkbox" id="darkmode-toggle" />
+                        <label
+                            htmlFor="darkmode-toggle"
+                            className="darkmode-label relative block h-6 w-16 cursor-pointer rounded-full bg-lightGray-500 shadow-inner shadow-lightGray-200"
+                        >
+                            <BsSun className="darkmode-sun" />
+                            <BsMoon className="darkmode-moon" />
+                        </label>
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => console.log("login button clicked")}
+                            className="ml-4 px-4 py-2"
+                        >
+                            Login
+                        </button>
+                        <button
+                            onClick={() => console.log("signup button clicked")}
+                            className="ml-4 rounded-xl bg-blue-accent px-4 py-2 text-white hover:bg-blue-dark"
+                        >
+                            Signup
+                        </button>
+                    </div>
                 </div>
             </header>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        colors: {
+            darkGray: {
+                100: "#18191B",
+                200: "#141416",
+                300: "#222529",
+                400: "#23262F",
+                500: "#282832",
+                600: "#3B3E44",
+                700: "#4A4E54",
+            },
+            lightGray: {
+                100: "#84878B",
+                200: "#92929A",
+                300: "#B1B5C3",
+                400: "#E7ECF3",
+                500: "#F4F5F6",
+                600: "#FCFCFD",
+                700: "#FAFAFA",
+                800: "#f5f5f5",
+                900: "#777E90",
+                1000: "#FCFCFD",
+            },
+            blue: {
+                accent: "#316BFF",
+                dark: "#0047FD",
+                transparent: "#316BFF33",
+            },
+            white: "#fff",
+            black: "#000",
+            transparentDark: "#14141633",
+            redPrimary: "#FF543D",
+            starGold: "#FFD166",
+            modalBg: "#36363696",
+        },
+        extend: {},
+    },
+    plugins: [],
 };
 
 // Test line for test commit
