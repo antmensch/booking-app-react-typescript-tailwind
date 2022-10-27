d358f148b4f71c7b68007e1e2d24d19433ceb1fb
Anton Sydorenko <an.sydorenko@gmail.com>
2022-10-27 23:26:41 +0300

    finished the header, fixed the user menu behaviour. Added some comments to the Header component.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { Dispatch, SetStateAction } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../../public/logo.svg";
 import { BsSun, BsMoon, BsArrow90DegDown } from "react-icons/bs";
 import { GoTriangleDown } from "react-icons/go";
+import { FiUser } from "react-icons/fi";
+import { RiSuitcaseLine, RiLogoutCircleLine } from "react-icons/ri";
 import useUser from "../../hooks/useUser";
 
 interface HeaderProps {
@@ -13,11 +15,24 @@ interface HeaderProps {
 }
 
 function Header({ colorTheme, setColorTheme }: HeaderProps) {
+    // fetching user data using the useUser custom hook
     const { user, isLoading: userLoading } = useUser();
-    const [userMenuState, setUserMenuState] = useState("closed");
+
+    // state for the user menu
+    const [userMenuState, setUserMenuState] = useState<"open" | "closed">(
+        "closed"
+    );
+
+    // useEffect adding event listener to the window so that user menu closes when the window is clicked
+    useEffect(() => {
+        window.addEventListener("click", () => {
+            setUserMenuState("closed");
+        });
+    }, []);
     return (
         <div className="fixed top-0 left-0 flex w-full items-center text-xl font-bold dark:bg-darkGray-300 sm:h-20">
             <header className="container mx-auto flex flex-col justify-between py-2 sm:flex-row sm:py-0">
+                {/* Logo */}
                 <Link href="/">
                     <a className="flex items-center justify-center">
                         <Image src={logo} width="40px" height="40px" />
@@ -26,6 +41,7 @@ function Header({ colorTheme, setColorTheme }: HeaderProps) {
                         </span>
                     </a>
                 </Link>
+                {/* Night mode toggle */}
                 <div className="mt-1 flex flex-col-reverse items-center justify-center sm:mt-0 sm:flex-row sm:justify-between">
                     <div className="mr-4 flex items-center justify-center">
                         <input
@@ -46,7 +62,9 @@ function Header({ colorTheme, setColorTheme }: HeaderProps) {
                             <BsMoon className="darkmode-moon" />
                         </label>
                     </div>
+                    {/* TODO: Update when auth is implemented */}
                     {(
+                        // Login and Signup buttons
                         <div>
                             <button
                                 onClick={() =>
@@ -66,23 +84,74 @@ function Header({ colorTheme, setColorTheme }: HeaderProps) {
                             </button>
                         </div>
                     ) &&
+                        // User menu rendered if the user is logged in
                         (userLoading ? (
                             <span className="text-xs">Loading...</span>
                         ) : (
-                            <div className="relative flex h-full w-48 cursor-pointer flex-row items-center justify-around px-2">
-                                <div className="h-8 w-8 overflow-hidden rounded-full">
-                                    <Image
-                                        src={user.picsrc}
-                                        height={32}
-                                        width={32}
-                                        objectFit="fill"
-                                        className="scale-125"
+                            <div className="relative">
+                                <div
+                                    className="flex h-full w-48 cursor-pointer flex-row items-center justify-around px-2"
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        if (userMenuState === "open")
+                                            setUserMenuState("closed");
+                                        else setUserMenuState("open");
+                                    }}
+                                >
+                                    <div className="h-8 w-8 overflow-hidden rounded-full">
+                                        <Image
+                                            src={user.picsrc}
+                                            height={32}
+                                            width={32}
+                                            objectFit="fill"
+                                            className="scale-125"
+                                        />
+                                    </div>
+                                    <div className="whitespace-nowrap font-roboto text-sm font-medium dark:text-white">
+                                        {user.name}
+                                    </div>
+                                    <GoTriangleDown
+                                        className={`cursor-pointer text-darkGray-600 dark:text-white ${
+                                            userMenuState === "open" &&
+                                            "rotate-180 transition-transform"
+                                        }`}
                                     />
                                 </div>
-                                <div className="whitespace-nowrap font-roboto text-sm font-medium dark:text-white">
-                                    {user.name}
-                                </div>
-                                <GoTriangleDown className="cursor-pointer text-darkGray-600 dark:text-white" />
+
+                                {/* User menu */}
+                                <ul
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                    }}
+                                    className={`text-md absolute right-0 top-12 w-48 cursor-default list-none space-y-4 overflow-hidden rounded-2xl bg-white p-4 font-dmsans text-sm font-medium text-darkGray-400 opacity-100 shadow-md transition-all dark:bg-darkGray-400 dark:text-white ${
+                                        userMenuState === "closed" && "h-0 py-0"
+                                    }`}
+                                >
+                                    <li className="hover:text-lightGray-1 cursor-pointer transition">
+                                        <Link href={"/"}>
+                                            <a>
+                                                <FiUser className="text-lightGray-8 mr-3 inline-block" />
+                                                <span>My Profile</span>
+                                            </a>
+                                        </Link>
+                                    </li>
+
+                                    <li className="hover:text-lightGray-1 cursor-pointer transition">
+                                        <Link href={"/"}>
+                                            <a>
+                                                <RiSuitcaseLine className="text-lightGray-8 mr-3 inline-block" />
+                                                <span>Bookings</span>
+                                            </a>
+                                        </Link>
+                                    </li>
+
+                                    <li className="hover:text-lightGray-1 cursor-pointer transition">
+                                        <a>
+                                            <RiLogoutCircleLine className="text-lightGray-8 mr-3 inline-block" />
+                                            <span>Sign Out</span>
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
                         ))}
                 </div>
