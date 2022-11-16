0223a79f3af702d6f7f40cbb50f735435b749b88
Anton Sydorenko <an.sydorenko@gmail.com>
2022-11-16 20:04:39 +0200

    minor bugs

--- src/components/home/best-place/BestPlaceCard.tsx
+++ src/components/home/best-place/BestPlaceCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { BestPlace } from "./useBestPlace";
+import { BestPlace } from "./BestPlacesSection";
 
 function BestPlaceCard({ img, country, destinations }: BestPlace) {
     return (
